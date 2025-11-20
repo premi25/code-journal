c26ebf55c8db8c6b473e9cf7e4b37f5703d5acde
PREMI <premijaya04moorthy@gmail.com>
2025-11-20 12:38:28 +0530

    init commit

---
+++ src/recursion.java
@@ -0,0 +1,98 @@
+//public class recursion {
+//
+//    void num (int n ){
+//
+//       if(n>=5 ){
+//          return;
+//       }
+//       num( n+1);
+//        System.out.println(n );
+//    }
+//    public static void main(String[] args) {
+//      recursion re = new recursion();
+//      re.num( 1);
+//    }
+//}
+
+//  print  name n  times  using recursion
+//public class recursion {
+//    void  name ( int i , int n ){
+//        if( i >  n ){
+//               return ;
+//        }
+//        System.out.println(" premi");
+//        name(i + 1, n);
+//    }
+//    public static void main(String[] args) {
+//     recursion recr =new recursion();
+//     recr.name(1,7);
+//    }
+//}
+
+//  print  lineraly from  1 to N
+
+//public  class recursion {
+//    void  num ( int i, int n ){
+//        if( i>n){
+//            return  ;
+//
+//        }
+//        System.out.println(i);
+//        num(i+1,n);
+//        System.out.println(i);
+//    }
+//
+//    public static void main(String[] args) {
+//
+//        recursion recr =new recursion();
+//          recr.num(1,10);
+//
+//    }
+//
+//}
+
+
+//  print  lineraly from  1 to  n using   back tarcking
+
+//public  class recursion {
+//    void  num ( int i, int n ){
+//        if( i<1){
+//            return  ;
+//
+//        }
+////        System.out.println(i);
+//        num(--i,n);
+//        System.out.println(i);
+//    }
+//
+//    public static void main(String[] args) {
+//
+//        recursion recr =new recursion();
+//          recr.num(7,7);
+//
+//    }
+//
+//}
+//
+
+////  sum  of  natural  numbers
+//
+//public class recursion {
+//      void sum( int i, int n ) {
+//          if (i > n) {
+//              return;
+//          }
+//          int tot = 0 + i;
+//
+//          sum(i + 1, n);
+//      }
+//
+//    public static void main(String[] args) {
+//        recursion recr =new recursion();
+//        recr.sum(1,7);
+//
+//
+//    }
+//}
+
+//
---
+++ src/reverse.java
@@ -0,0 +1,42 @@
+
+
+
+//import java.util.Scanner;
+//
+//public class reverse {
+//    public static void main(String[] args) {
+//
+//        System.out.println(" enter a  string ");
+//        Scanner scan = new Scanner(System.in);
+//        String str = scan.next();
+//        String rev = "";
+//        for (int i = str.length() - 1; i >= 0; i--) {
+//            rev = rev + str.charAt(i);
+//        }
+//        System.out.println(rev +" "+ str);
+//    }
+//
+//    }
+//
+
+
+//import java.util.Scanner;
+//
+//public class reverse {
+//    public static void main(String[] args) {
+//        System.out.println("enter a  string:");
+//        Scanner sc = new Scanner(System.in);
+//        String str = sc.next();
+//
+//        for(int i =0; i<=str.length()-1; i++){
+//
+//
+//
+//
+//            }
+//        }
+//
+//
+//
+//    }
+//}
